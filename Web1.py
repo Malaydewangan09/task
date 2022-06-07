99544246268ebfb49e6060b86f2f230921915ade
malay <malaydewangan310@gmail.com>
2022-06-07 16:45:32 +0530

    initial

--- Web1.py
+++ Web1.py
@@ -1,1 +1,32 @@
+from bs4 import BeautifulSoup
+import requests,openpyxl
 
+
+excel1 = openpyxl.Workbook()
+sheet =excel1.active 
+sheet.append(['Name','Price'])
+
+source  = requests.get('https://healthybuddha.in/fruits-vegetables/fresh-fruits')
+soup = BeautifulSoup(source.text ,'html.parser')
+
+
+names = soup.find('div',id = "resp-banner-tab").find('div',class_="col-xs-12 p-b5 web-category-product-div").find('div',class_="tab-content col-lg-12").find('section',id="sidebar-main").find('div',id="content").find('div',id = "products").find('div',class_="products-block").find_all('div',class_="col-md-2 col-sm-6 text-center text-muted text-capitalize p0 product-col")
+
+
+
+for name in names:
+    
+    fruitname = name.find('div').find('div',class_="product-meta resp-product-data").div.find('div',class_="left product-div-box").div.p.a.text  
+
+
+    price = name.find('div').find('div',class_="product-meta resp-product-data").div.find('div',class_="right resp-product-right").find('div',class_="action resp-product-action").find('div').find('div').find('select').option.text
+    print(price)
+    break
+    sheet.append([fruitname,price])
+
+
+
+
+
+excel1.save("fruit-data1.xlsx")
+    
--- Web2.py
+++ Web2.py
@@ -1,1 +1,33 @@
 
+from bs4 import BeautifulSoup
+import requests,openpyxl
+
+
+
+excel = openpyxl.Workbook()
+sheet =excel.active
+sheet.append(['Name' , 'Price','Stock-left'])
+
+
+
+source  = requests.get('https://communityfarm.in/product-category/fruits/')
+soup = BeautifulSoup(source.text ,'html.parser')
+
+name = soup.find('div',class_="communityFarm").find('div',class_="container-fluid").find('div',class_="mainRow").find('div',class_="content-area shop-page shop-has-sidebar").find('div',class_ = "row").find('div').find('div').find('div').find('div',class_="xlarge-10 large-9 columns").find_all('div',class_='row')
+
+
+
+fruitlist = name[3].find('div').find('ul').find_all('li')
+
+
+
+for fruit in fruitlist:
+    fruitname = fruit.find('div').find('div',class_="productName").a.text
+    price = fruit.find('div').find('form').find('div',class_="pricesGrids").find('div',class_="aligTxt float-left").span.bdi.text 
+    stock = fruit.find('div').find('form').find('div',class_="pricesGrids").find('div',class_="aligTxt float-right").p.text
+    sheet.append([fruitname , price,stock])
+    
+
+
+
+excel.save("fruits-data.xlsx")
